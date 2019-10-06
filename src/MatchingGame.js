a207574f5b6fd48e6b03420916a0cb97d2f8482a
Emily Pillay <epillay221@gmail.com>
2019-10-06 15:23:14 -0400

    created basic matching game

--- src/MatchingGame.js
+++ src/MatchingGame.js
@@ -1,71 +1,80 @@
 import React from 'react';
 import './MatchingGame.css';
 import { all } from 'q';
+import { useState } from 'react';
+import { isProperty, tsPropertySignature } from '@babel/types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const images = ["https://rb.gy/a4ea6c", "https://rb.gy/aeb84b", "https://rb.gy/b783eb"];
 
-function MatchingGame() {
-  return (
-    <div className="MatchingGameContainer">
-        {renderImageToMatch()}
-        {renderOtherImages()}
-    </div>
-  );
+const images = ["coffee", "rocket", "check-square", "piggy-bank"];
+
+class MatchingGame extends React.Component {
+    constructor(props) {
+        super(props);
+        let randImage = this.getRandomImage();
+        this.state = {
+          topImage: randImage,
+        };
+
+        this.otherImages = images.filter(image => image !== this.state.topImage);
+    }
+
+    renderImage(image) {
+        return (
+        <div className="otherImage" >
+            <FontAwesomeIcon icon={image} onClick={() => this.handleClick(image)}/>
+        </div>)
+    }
+
+    handleClick(image) {
+        if (image === this.state.topImage) {
+            console.log("YAY");
+        } else {
+            console.log("BOO");
+        }
+        this.refresh(this.state.topImage);
+    }
+
+    refresh() {
+        const randImage = this.getRandomImage();
+        this.setState({topImage : randImage});
+        this.setOtherImages(randImage);
+    }
+
+    setOtherImages(topImage) {
+        console.log("Unfiltered other: " + images);
+        console.log("Image to remove " + topImage);
+        let filtered = images.filter(image => image !== topImage);
+        console.log("Filtered other: " + filtered);
+        this.otherImages = filtered;
+        
+    }
+
+    shuffle(arrayOfImages) {
+        return arrayOfImages.sort(() => Math.random() - 0.5);
+    }
+
+    getRandomImage() {
+        return images[Math.floor(Math.random()*images.length)];
+    }
+    render() {
+        this.shuffle(this.otherImages);
+        let allImages = this.shuffle([this.state.topImage, this.otherImages[0], this.otherImages[1]]);
+        console.log("All Images: " + allImages);
+        return (
+            <div className="matchingGameContainer">
+                <div className="topImage">
+                    <FontAwesomeIcon icon={this.state.topImage}/>
+                </div>
+                <div className="otherImagesContainer">
+                    {this.renderImage(allImages[0])}
+                    {this.renderImage(allImages[1])}
+                    {this.renderImage(allImages[2])}
+                </div>
+            </div>
+        );
+    }
 }
 
-/**
- * Renders the top image for the user to match to
- */
-function renderImageToMatch() {
-    // TODO: Change rendering to use image from state
-    return (
-        <div className="topImage">
-            <img src={getImageToMatch()}/>
-        </div>
-      );
-}
-
-/**
- * Renders the bottom three images for the user to choose from
- */
-function renderOtherImages() {
-    // TODO: Change to use state
-    const randomImages = [, ]
-    const allImages = [getImageToMatch(), randomImages[0], randomImages[1]];
-    shuffle(allImages);
-    return (
-        <div className="otherImages">
-            <img src={allImages[0]}/>
-            <img src={allImages[1]}/>
-            <img src={allImages[2]}/>
-        </div>
-      );
-}
-
-/**
- * Returns the image to match
- */
-function getImageToMatch() {
-    // TODO: Use React Hooks to set this to state
-    return "https://rb.gy/335db0";
-}
-
-/**
- * Returns a random image
- */
-function getRandomImages() {
-    const imagesToMatch = getImageToMatch();
-
-    const randomNumber = Math.floor(Math.random() * (4+1));
-    return images[randomNumber];
-
-    // Loop throuhg and find 2 different images that are also not the image to match
-    // Return them in an array
-    // Comment out previous test function
-}
-
-function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-}
 
 export default MatchingGame;
