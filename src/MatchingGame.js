74ed5f4f8c74c0b43d3b37a9f87bbb49049a7a3a
Arun Jeevanantham <itsmearun98@gmail.com>
2019-10-06 13:36:29 -0400

    test

--- src/MatchingGame.js
+++ src/MatchingGame.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import './MatchingGame.css';
-import { all } from 'q';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const images = ["https://rb.gy/a4ea6c", "https://rb.gy/aeb84b", "https://rb.gy/b783eb"];
+const images = ["coffee", "rocket", "check-square", "piggy-bank"];
 
 function MatchingGame() {
+
+  const [imageToMatch, setImageToMatch] = useState(getImageToMatch());
+  
   return (
-    <div className="MatchingGameContainer">
-        {renderImageToMatch()}
-        {renderOtherImages()}
+    <div className="matchingGameContainer">
+        {renderImageToMatch(imageToMatch)}
+        {renderOtherImages(imageToMatch, setImageToMatch)}
     </div>
   );
 }
@@ -16,11 +19,10 @@ function MatchingGame() {
 /**
  * Renders the top image for the user to match to
  */
-function renderImageToMatch() {
-    // TODO: Change rendering to use image from state
+function renderImageToMatch(imageToMatch) {
     return (
         <div className="topImage">
-            <img src={getImageToMatch()}/>
+            <FontAwesomeIcon icon={imageToMatch} />
         </div>
       );
 }
@@ -28,40 +30,71 @@ function renderImageToMatch() {
 /**
  * Renders the bottom three images for the user to choose from
  */
-function renderOtherImages() {
+function renderOtherImages(imageToMatch, setImageToMatch) {
     // TODO: Change to use state
-    const randomImages = [, ]
-    const allImages = [getImageToMatch(), randomImages[0], randomImages[1]];
+    const randomImages = getRandomImages(imageToMatch);
+    const allImages = [imageToMatch, randomImages[0], randomImages[1]];
     shuffle(allImages);
     return (
-        <div className="otherImages">
-            <img src={allImages[0]}/>
-            <img src={allImages[1]}/>
-            <img src={allImages[2]}/>
+        <div className="otherImagesContainer">
+            <div 
+                className="otherImage"
+                onClick={handleOnClick.bind(this, imageToMatch, allImages[0], setImageToMatch)}
+            >
+                <FontAwesomeIcon icon={allImages[0]} />
+            </div>
+            <div 
+                className="otherImage"
+                onClick={handleOnClick.bind(this, imageToMatch, allImages[1], setImageToMatch)}>
+                <FontAwesomeIcon icon={allImages[1]} />
+            </div>
+            <div 
+                className="otherImage"
+                onClick={handleOnClick.bind(this, imageToMatch, allImages[2], setImageToMatch)}>
+                <FontAwesomeIcon icon={allImages[2]} />
+            </div>
         </div>
       );
+}
+
+function handleOnClick(imageToMatch, currentImage, setImageToMatch) {
+    if (currentImage === imageToMatch) {
+        console.log("YAY")
+    } else {
+        console.log("BOO")
+    }
+    setImageToMatch(getImageToMatch)
 }
 
 /**
  * Returns the image to match
  */
 function getImageToMatch() {
-    // TODO: Use React Hooks to set this to state
-    return "https://rb.gy/335db0";
+    const randomNumber = Math.floor(Math.random() * (images.length));
+    return images[randomNumber];
 }
 
 /**
- * Returns a random image
+ * Returns a list of two random images that doesn't include the image to match.
  */
-function getRandomImages() {
-    const imagesToMatch = getImageToMatch();
+function getRandomImages(imageToMatch) {
+    console.log(imageToMatch)
+    console.log(images);
+    const imagesWithoutImageToMatch = images.filter(function(value, index, arr) {
+        return value !== imageToMatch;
+    });
+    console.log(imagesWithoutImageToMatch)
+    const randomNumber = Math.floor(Math.random() * (imagesWithoutImageToMatch.length));
+    let randomNumber2 = Math.floor(Math.random() * (imagesWithoutImageToMatch.length));
+    if (randomNumber2 === randomNumber) {
+        if (randomNumber2 === imagesWithoutImageToMatch.length - 1) {
+            randomNumber2--;
+        } else {
+            randomNumber2++;
+        }
+    }
 
-    const randomNumber = Math.floor(Math.random() * (4+1));
-    return images[randomNumber];
-
-    // Loop throuhg and find 2 different images that are also not the image to match
-    // Return them in an array
-    // Comment out previous test function
+    return [imagesWithoutImageToMatch[randomNumber], imagesWithoutImageToMatch[randomNumber2]];
 }
 
 function shuffle(array) {
