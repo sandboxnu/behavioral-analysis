b56475fc448228e8ff7bb784fa26b26edf6db8ab
Emily Pillay <epillay221@gmail.com>
2019-10-10 18:38:49 -0400

    warning system added

--- src/Experiment.js
+++ src/Experiment.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Launch.css';
+import './Experiment.css';
 import MatchingGame from './MatchingGame.js';
+import styled from 'styled-components'
+import Warning from './Warning.js';
+
+
 
 function Experiment() {
   return (
-    <div className="ExperimentContainer">
+    <div className="experimentContainer" id='experimentContainer'>
+        <Warning
+          condition={"A"}/>
         <MatchingGame/>
     </div>
   );
