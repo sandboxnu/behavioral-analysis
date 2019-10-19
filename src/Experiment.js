996e5d7eaf4712ef7c6410abacf698c349da7520
Karmen <lu.kar@husky.neu.edu>
2019-10-19 19:36:57 -0400

    Style score to be in top right corner of matching game card.

--- src/Experiment.js
+++ src/Experiment.js
@@ -5,7 +5,25 @@ import MatchingGame from './MatchingGame.js';
 import styled from 'styled-components'
 import Warning from './Warning.js';
 
-
+var divStyle = {
+    float: 'right',
+    color: 'white',
+    backgroundColor: 'lightsteelblue',
+    display: 'inline-block',
+    width: '190px',
+    height: '80px',
+    padding: '0.7em 1.4em',
+    borderRadius: '0.1em',
+    textAlign: 'center',
+    position: 'absolute',
+    textTransform: 'uppercase',
+    fontWeight: '400',
+    fontSize: '22px',
+    top: '0',
+    right: '0',
+    fontFamily: 'Roboto'
+};
+  
 
 class Experiment extends React.Component {
     constructor(props) {
@@ -26,7 +44,7 @@ class Experiment extends React.Component {
         <div className="experimentContainer" id='experimentContainer'>
             <Warning
                 condition={"A"} parentCallback={this.scoreDeltaCallback}/>
-            <div>Score: {this.state.score}</div>
+            <div style={divStyle}>Score: {this.state.score}</div>
             <MatchingGame parentCallback={this.scoreDeltaCallback}/>
         </div>
     );}
