cd3647e567fe599913d96e49a0506f392d863509
Karmen <lu.kar@husky.neu.edu>
2019-11-11 14:07:22 -0500

    Implement admin configuration panel.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Launch from './components/Launch';
+import AdminPanel from './components/AdminPanel';
 import * as serviceWorker from './serviceWorker';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -11,7 +12,7 @@ library.add(fab, faCheckSquare, faCoffee, faRocket, faPiggyBank)
 
 console.log(faPiggyBank.iconName);
 
-ReactDOM.render(<Launch />, document.getElementById('root'));
+ReactDOM.render(<AdminPanel />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
