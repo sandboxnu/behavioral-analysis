f7df26a62a081e2d0f9d10354234c8e35941eb9a
Karmen <lu.kar@husky.neu.edu>
2019-11-25 19:45:26 -0500

    Create basic end game screen.

--- src/components/App.js
+++ src/components/App.js
@@ -2,34 +2,37 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import UserSignIn from './UserSignIn';
 import AdminPanel from './AdminPanel';
+import End from './End';
 
 function AppRouter() {
     return (
         <Router>
             <div className='App'>
-                <Route path='/admin' exact component={AdminPanel}/>
-                <Route
-                    path='/a'
-                    render={(props) => <UserSignIn {...props} condition={"A"} />}
-                />
-                <Route
-                    path='/b'
-                    render={(props) => <UserSignIn {...props} condition={"B"} />}
-                />
-                <Route
-                    path='/c'
-                    render={(props) => <UserSignIn {...props} condition={"C"} />}
-                />
-                <Route
-                    path='/d'
-                    render={(props) => <UserSignIn {...props} condition={"D"} />}
-                />
-                <Route
-                    path='/' exact component={UserSignIn}
-                />
+               <End/>
             </div>
         </Router>
     );
 }
 
 export default AppRouter;
+
+{/* <Route path='/admin' exact component={AdminPanel}/>
+<Route
+    path='/a'
+    render={(props) => <UserSignIn {...props} condition={"A"} />}
+/>
+<Route
+    path='/b'
+    render={(props) => <UserSignIn {...props} condition={"B"} />}
+/>
+<Route
+    path='/c'
+    render={(props) => <UserSignIn {...props} condition={"C"} />}
+/>
+<Route
+    path='/d'
+    render={(props) => <UserSignIn {...props} condition={"D"} />}
+/>
+<Route
+    path='/' exact component={UserSignIn}
+/> */}
---
+++ src/components/End.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Experiment from './Experiment';
+import styled from 'styled-components';
+
+const Screen = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Title = styled.div`
+  margin-bottom: 0.25em;
+  font-size: 4em;
+  color: #f7b733;
+`
+const Subtitle = styled.div`
+  margin-bottom: 1.5em;
+  font-size: 1em;
+  margin: auto; 
+  width: 30em;
+  margin-bottom: 2em;
+  color: #393e47;
+`
+const ScreenContainer = styled.div`
+  margin: auto;
+  text-align: center;
+`
+const InfoBox = styled.div`
+  background-color: #f7b733;
+  color: white;
+  padding: .5em;
+  font-size: 1.2em;
+  border-radius: .5em;
+  align: center;
+  margin: auto;
+  width: 15em;
+`
+
+class End extends Component {
+  render() {
+    let daScore = 5;
+    let daString = "turkey";
+    return (
+      <Screen>
+        <ScreenContainer>
+          <Title> Behavioral Analysis</Title>
+          <Subtitle>You've reached the end!<br />Please contact your researcher.</Subtitle>
+          <InfoBox>Score:{' '}{daScore}<br />Configuration String:{' '}{daString}</InfoBox>
+        </ScreenContainer>
+      </Screen>
+    );
+  }
+}
+
+
+export default End;
