379c41b29d4602a9bb6a1b8af747eeb463e36d33
Arun Jeevanantham <itsmearun98@gmail.com>
2019-12-03 11:24:25 -0500

    changes routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
