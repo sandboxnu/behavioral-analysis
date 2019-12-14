70f838252acf43d22fcaee09b67e6b8ebdf25bcf
Arun Jeevanantham <itsmearun98@gmail.com>
2019-12-13 21:56:27 -0500

    added support to downnload by user

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -14,66 +14,43 @@ import process from './dataAnalysis.js';
 const SERVER_URL = ServerUtils.getServerUrl();
 
 const PanelContainer = styled.div`
-    padding-top: '25',
-    padding-bottom: '25'
+  padding-top: '25',
+  padding-bottom: '25'
 `;
 
 const DownloadButton = styled.button`
-    margin-top: '5',
-    margin-bottom: '20'
+  margin-top: '5',
+  margin-bottom: '20'
 `;
 
 class UserIdForm extends Component {
     constructor(props) {
-        super(props);
-        this.state = {value: ''};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+      super(props);
+      this.state = {value: ''};
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(e) {
-        this.setState({value: e.target.value});
+      this.setState({value: e.target.value});
     }
 
-    async getDataforUser(username, password, user) {
-        let data = Axios.get(`${SERVER_URL}/data/${user}`, {
-          auth: {
-            username: username,
-            password: password,
-          },
-        }).then(response => {
-          console.log(response)
-          return response;
-        })
-          .catch(error => console.log(error));
-        return data;
-      }
-
     handleSubmit(e) {
-        console.log(this.state.value);
-        e.preventDefault();
-
-        this.getDataforUser(this.props.username, this.props.password, e).then((response) => {
-            let processedData = [];
-            for (let i = 0; i < response.data.length; i++) {
-                processedData.push(process(response.data[i].sessiondata, response.data[i].participantid));
-            }
-
-            CSVUtils.JSONToCSVConvertor(processedData, 'Behavior Analysis Data', true);
-         } );
+      e.preventDefault();
+      this.props.downloadDataForUser(this.state.value);
     }
 
     render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                
-                <input type="text" value={this.state.value} onChange={this.handleChange}/>{' '}
-                <DownloadButton type="submit" className="btn btn-primary">
-                    <FontAwesomeIcon icon={faDownload}/>{' '}
-                    Download data for given userId
-                </DownloadButton>
-            </form>
-        );
+      return (
+          <form onSubmit={this.handleSubmit}>
+              
+              <input type="text" value={this.state.value} onChange={this.handleChange}/>{' '}
+              <DownloadButton type="submit" className="btn btn-primary">
+                  <FontAwesomeIcon icon={faDownload}/>{' '}
+                  Download data for given userId
+              </DownloadButton>
+          </form>
+      );
     }
 }
 
@@ -90,41 +67,27 @@ class AdminPanel extends Component {
     }
 
     onSubmit({ formData }) {
-        Axios.post(`${SERVER_URL}/config`, formData, {
-            auth: {
-              username: this.state.username,
-              password: this.state.password,
-            },
-          }).then(() => this.setState({ configOnServer: formData }))
-            .catch(error => console.log(error));
-    }
-
-    async getData(username, password) {
-        let data = Axios.get(`${SERVER_URL}/data`, {
+      Axios.post(`${SERVER_URL}/config`, formData, {
           auth: {
-            username: username,
-            password: password,
+            username: this.state.username,
+            password: this.state.password,
           },
-        }).then(response => {
-          console.log(response)
-          return response;
-        })
+        }).then(() => this.setState({ configOnServer: formData }))
           .catch(error => console.log(error));
-        return data;
     }
 
     onChange({ formData }) {
-        this.setState({
-            formData
-        });
+      this.setState({
+          formData
+      });
     }
 
     onServerData(data) {
-        console.log(data);
-        this.setState({
-            configOnServer: data,
-            formData: data,
-        });
+      console.log(data);
+      this.setState({
+          configOnServer: data,
+          formData: data,
+      });
     }
 
     onClose(e) {
@@ -144,16 +107,26 @@ class AdminPanel extends Component {
       });
     }
 
-    downloadData() { 
-        this.getData(this.state.username, this.state.password).then((response) => {
-            let processedData = [];
-            for (let i = 0; i < response.data.length; i++) {
-                processedData.push(process(response.data[i].sessiondata, response.data[i].participantid));
-            }
-            processedData.sort((a, b) => (a.participantid < b.participantid) ? 1 : -1)
+    downloadData(response) { 
+      let processedData = [];
+        for (let i = 0; i < response.data.length; i++) {
+            processedData.push(process(response.data[i].sessiondata, response.data[i].participantid));
+        }
+        processedData.sort((a, b) => (a.participantid < b.participantid) ? 1 : -1)
 
-            CSVUtils.JSONToCSVConvertor(processedData, 'Behavior Analysis Data', true);
-         } );
+        CSVUtils.JSONToCSVConvertor(processedData, 'Behavior Analysis Data', true);
+    }
+
+    downloadAllData() {
+      ServerUtils.getData(this.state.username, this.state.password).then((response) => {
+        this.downloadData(response);
+      });
+    }
+
+    downloadDataForUser = (user) => {
+      ServerUtils.getDataforUser(this.state.username, this.state.password, user).then((response) => {
+        this.downloadData(response);
+      });
     }
 
     renderPanel() {
@@ -164,7 +137,7 @@ class AdminPanel extends Component {
                 <Fetch url={`${SERVER_URL}/config`} as="json" onDataChange={d => this.onServerData(d)}>
                     <PanelContainer className="panel container">
                         <h2>Download all collected data</h2>
-                        <DownloadButton onClick={() => { this.downloadData(); }} type="button" className="btn btn-primary">
+                        <DownloadButton onClick={() => { this.downloadAllData() }} type="button" className="btn btn-primary">
                             <FontAwesomeIcon icon={faDownload}/>
                             {' '}
                             Download all collected data
@@ -173,7 +146,8 @@ class AdminPanel extends Component {
                         <h2>Download data for given userId</h2>
                         <UserIdForm
                             username={this.state.username}
-                            password={this.state.password}/>
+                            password={this.state.password}
+                            downloadDataForUser={this.downloadDataForUser}/>
                         <hr/>
                         <h2>Configure Experiment</h2>
                         <Form   
@@ -190,16 +164,16 @@ class AdminPanel extends Component {
     }
 
     render() {
-        if (!this.state.authenticated) {
-          return (
-            <div>
-              <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"/>
-              <Login onLogin={this.onLogin.bind(this)} />
-            </div>
-          );
-        }
-        return this.renderPanel();
+      if (!this.state.authenticated) {
+        return (
+          <div>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous"/>
+            <Login onLogin={this.onLogin.bind(this)} />
+          </div>
+        );
       }
+      return this.renderPanel();
+    }
 
 }
 
